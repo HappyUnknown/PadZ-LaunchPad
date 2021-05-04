4035d492c83c0dfa7cc05dbb2592d8e0b8d874ca
HappyUnknown <39666812+HappyUnknown@users.noreply.github.com>
2021-05-04 13:27:16 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PadZ
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string setPath = "settings.txt";
+        const string SPEC = "~";
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (!System.IO.File.Exists(setPath)) System.IO.File.Create(setPath);
+            RefreshList();
+        }
+        char GetSpec() { return SPEC[0]; }
+        void RefreshList()
+        {
+            liPads.Items.Clear();
+            foreach (var set in System.IO.File.ReadAllLines(setPath))
+            {
+                liPads.Items.Add(set.Replace(SPEC[0], ' '));
+            }
+        }
+        string FilesPath()
+        {
+            return Environment.CurrentDirectory + "\\AppFiles\\";
+        }
+        string[] RemoveBrokenSounds(string[] sets)
+        {
+            List<string> sounds = new List<string>();
+            try
+            {
+                MessageBox.Show(sets[0].Split(GetSpec())[2]);
+                foreach (string set in sets)
+                {
+                    string thispath = set.Split(GetSpec())[2];
+                    if (File.Exists(thispath))
+                    {
+                        sounds.Add(set);
+                    }
+                    else MessageBox.Show("File !exists " + set);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("RemoveBrokenSounds():" + ex.Message); }
+            return sounds.ToArray();
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCaption.Text.Length != 0 && tbColor.Text.Length != 0 && tbAudioPath.Text.Length != 0)
+            {
+                try
+                {
+                    System.IO.File.AppendAllText(setPath, tbCaption.Text + SPEC + tbColor.Text + SPEC + Environment.CurrentDirectory + "\\AppFiles\\" + GetFileName(tbAudioPath.Text) + Environment.NewLine);
+                    RefreshList();
+                    if (!Directory.Exists(Environment.CurrentDirectory + "\\AppFiles")) Directory.CreateDirectory(Environment.CurrentDirectory + "\\AppFiles");
+                    File.Copy(tbAudioPath.Text, Environment.CurrentDirectory + "\\AppFiles\\" + GetFileName(tbAudioPath.Text));
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+            }
+        }
+
+        private void liPads_Selected(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (liPads.SelectedIndex != -1)
+            {
+                try
+                {
+                    List<string> setli = System.IO.File.ReadAllLines(setPath).ToList();
+                    setli.RemoveAt(liPads.SelectedIndex);
+                    System.IO.File.WriteAllLines(setPath, setli.ToArray());
+                    RefreshList();
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+            }
+        }
+        private void btnChooseAudio_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            if (fd.ShowDialog() == true)
+            {
+                tbAudioPath.Text = fd.FileName;
+            }
+        }
+        private void btnMake_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.ReadAllLines(setPath).Length > 0)
+            {
+                try
+                {
+                    int lastlen = File.ReadAllLines(setPath).Length;
+                    File.WriteAllLines(setPath, RemoveBrokenSounds(File.ReadAllLines(setPath)));
+                    int finlen = File.ReadAllLines(setPath).Length;
+                    if (lastlen != finlen) MessageBox.Show("Pre-launch sound availbility check changed sounds count: " + lastlen + "=>" + finlen);
+                }
+                catch (Exception ex) { MessageBox.Show("btnMake_Click: " + ex.Message); }
+                new PadWindow().ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("There is no sense in creating empty launchpad.");
+            }
+        }
+        string GetFileName(string path)
+        {
+            for (int i = path.Length - 1; i >= 0; i--)
+            {
+                if (path[i] == '\\')
+                {
+                    return path.Substring(i + 1, path.Length - i - 1);
+                }
+            }
+            return "NO_NAME";
+        }
+        string FileToBase64(string path)
+        {
+            return Convert.ToBase64String(File.ReadAllBytes(path));
+        }
+        List<byte[]> Base64ToBytes(string[] base64s)
+        {
+            List<byte[]> bytes = new List<byte[]>();
+            for (int i = 0; i < base64s.Length; i++)
+            {
+                bytes.Add(Convert.FromBase64String(base64s[i]));
+            }
+            return bytes;
+        }
+        void CreateFile(string name = "")
+        {
+            try
+            {
+                byte[] ret = Convert.FromBase64String(name);
+                FileInfo fil = new FileInfo(name);
+                using (Stream sw = fil.OpenWrite())
+                {
+                    sw.Write(ret, 0, ret.Length);
+                    sw.Close();
+                }
+            }
+            catch
+            {
+            }
+        }
+
+    }
+}
---
+++ PadWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PadZ
+{
+    /// <summary>
+    /// Логика взаимодействия для PadWindow.xaml
+    /// </summary>
+    public partial class PadWindow : Window
+    {
+        MediaPlayer player = new MediaPlayer();
+        int queuePos = 0;
+        const string SPEC = "~";
+        string setPath = "settings.txt";
+        List<string> setts = new List<string>();
+        List<Button> butts = new List<Button>();
+        int ButtonIndex(object sender)
+        {
+            Button b = (Button)sender;
+            for (int i = 0; i < butts.Count; i++)
+            {
+                if (butts[i] == b) return i;
+            }
+            return -1;
+        }
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (QueueAvailable()) liQueue.Items.Add(GetPath(setts, sender));
+                MediaPlayer music = new MediaPlayer();
+                music.Open(new Uri(GetPath(setts, sender)));
+                music.Play();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        public PadWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                setts = File.ReadAllLines(setPath).ToList();
+                for (int i = 0; i < setts.Count; i++)
+                {
+                    Button b = new Button();
+                    b.Width = 20;
+                    b.Height = 20;
+                    b.Content = i + 1;
+                    b.Click += new RoutedEventHandler(button1_Click);
+                    butts.Add(b);
+                    pnlContent.Children.Add(b);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEnableQueue_Click(object sender, RoutedEventArgs e)
+        {
+            btnOfQueueSound.IsEnabled = !btnOfQueueSound.IsEnabled;
+        }
+        bool QueueAvailable()
+        {
+            return btnOfQueueSound.IsEnabled;
+        }
+        private void btnOfQueueSound_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (liQueue.Items.Count > 0)
+                {
+                    liQueue.Items.RemoveAt(liQueue.Items.Count - 1);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        List<string> GetListedQueue()
+        {
+            List<string> queue = new List<string>();
+            for (int i = 0; i < liQueue.Items.Count; i++)
+                queue.Add(liQueue.Items[i].ToString());
+            return queue;
+        }
+        private void btnPlayQueue_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var queue = GetListedQueue();
+                player.Open(new Uri(queue[queuePos].ToString()));
+                player.Play();
+                queuePos++;
+                player.MediaEnded += new EventHandler(MediaEnded);
+            }
+            catch (Exception ex) { MessageBox.Show("btnPlayQueue_Click(): " + ex.Message); }
+        }
+        void MediaEnded(object sender, EventArgs e)
+        {
+            try
+            {
+                if (queuePos < GetListedQueue().Count)
+                {
+                    player.Open(new Uri(liQueue.Items[queuePos].ToString()));
+                    player.Play();
+                    queuePos++;
+                }
+                else { queuePos = 0; MessageBox.Show("DONE"); }
+
+            }
+            catch (Exception ex) { MessageBox.Show("MediaEnded:Index - " + queuePos + ">=" + GetListedQueue().Count + ". " + ex.Message); }
+        }
+        string GetPath(List<string> setts, object sender)
+        {
+            return setts[ButtonIndex(sender)].Split(SPEC[0])[2];
+        }
+    }
+}
